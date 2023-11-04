d036e431c89e0127ebf9d59eab12f65d393d0c8e
SphMoraes <144505465+SphMoraes@users.noreply.github.com>
2023-11-04 17:59:56 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,34 +1,34 @@
-export default [
-    {
-        question: "quanto é 3 + 3?",
+  export default [
+      {
+        question: "Quanto é 1 + 2?",
         answers: [
-            { option: "3", correct: true },
-            { option: "2", correct: false },
-            { option: "4", correct: false },
+          { option: "3", correct: true },
+          { option: "2", correct: false },
+          { option: "4", correct: false },
         ],
-    },
-    {
-        question: "quanto é 5 x 2?",
+      },
+      {
+        question: "Quanto é 5 x 2?",
         answers: [
-            { option: "9", correct: false },
-            { option: "10", correct: true },
-            { option: "11", correct: false },
+          { option: "9", correct: false },
+          { option: "10", correct: true },
+          { option: "11", correct: false },
         ],
-    },
-    {
-        question: "quanto é 10 x 8?",
+      },
+      {
+        question: "Quanto é 10 x 8?",
         answers: [
-            { option: "800", correct: false },
-            { option: "90", correct: false },
-            { option: "80", correct: true },
+          { option: "800", correct: false },
+          { option: "90", correct: false },
+          { option: "80", correct: true },
         ],
-    },
-    {
-        question: "quanto é 7 x 7?",
+      },
+      {
+        question: "Quanto é 7 x 7?",
         answers: [
-            { option: "70", correct: false },
-            { option: "56", correct: false },
-            { option: "49", correct: true },
+          { option: "70", correct: false },
+          { option: "56", correct: false },
+          { option: "49", correct: true },
         ],
-    },
-];
+      },
+    ];
